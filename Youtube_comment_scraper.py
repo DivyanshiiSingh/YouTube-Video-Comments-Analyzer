2d67140e00fda2cca17188e09cb9d8a541832aed
DivyanshiiSingh <155948465+DivyanshiiSingh@users.noreply.github.com>
2025-02-20 20:34:37 +0530

    Update Youtube_comment_scraper.py

--- Youtube_comment_scraper.py
+++ Youtube_comment_scraper.py
@@ -1,29 +1,27 @@
 from googleapiclient.discovery import build
 
-def scrapeComments(api_key, video_id) :
-
-  youtube = build('youtube', 'v3', developerKey=api_key)
+def scrapeComments(API_Key, video_id):
+  try:
+    youtube = build('youtube', 'v3', developerKey=API_Key)
+  except Exception as e:
+    print(f"Error building API : {e}")
+    raise
 
   comments = []
-  results = youtube.commentThreads().list(
-      part='snippet',
-      videoId=video_id,
-      textFormat='plainText',
-  ).execute()
+  try:
+    results = youtube.commentThreads().list(part='snippet', videoId=video_id, textFormat='plainText',).execute()
+  except Exception as e:
+    print(f"Error getting comments : {e}")
+    results = None
 
   while results:
       for item in results['items']:
           comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
           comments.append(comment)
 
-      if 'nextPageToken' in results:    # check for more comments in next page
+      if 'nextPageToken' in results:
           results = youtube.commentThreads().list(
-              part='snippet',
-              videoId=video_id,
-              textFormat='plainText',
-              pageToken=results['nextPageToken']
-          ).execute()
+              part='snippet', videoId=video_id, textFormat='plainText', pageToken=results['nextPageToken']).execute()
       else:
           break
   return comments
-
