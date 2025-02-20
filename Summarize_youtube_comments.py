b5ec47eabd361cfe326d9f44247dfbff1737b073
DivyanshiiSingh <155948465+DivyanshiiSingh@users.noreply.github.com>
2025-02-20 21:18:02 +0530

    Changed summarization model

--- Summarize_youtube_comments.py
+++ Summarize_youtube_comments.py
@@ -1,38 +1,35 @@
-from typing import final
-from transformers import pipeline
+from transformers import pipeline, AutoConfig, AutoTokenizer
 from Youtube_comment_scraper import scrapeComments
 
 def summarizeComments(API_Key, video_id):
-  comments = scrapeComments('API_Key', 'video_id')
-  concatenated_comments = " ".join(comments)
-  
-  summarizer = pipeline("summarization")
-  
-  # truncating the input text to a smaller size that is sure to be within the model's processing limit
-  max_input_length = summarizer.model.config.max_position_embeddings - 2
-  print(f'max_input_length : {max_input_length}, type : {type(max_input_length)}')
-  
-  try:
-    to_summarize = concatenated_comments
-    last_summary = ""
-    curr_summary = ""
-    final_summary = ""
-    i = 1
-  
-    while len(to_summarize) > 0:
-        to_summarize = to_summarize + last_summary
-  
-        if len(to_summarize) <= max_input_length:
-            final_summary = summarizer(to_summarize, max_length=100, min_length=5, do_sample=False)[0]['summary_text']
-            break
-  
-        curr_summary = summarizer(to_summarize[0: max_input_length], max_length=100, min_length=5, do_sample=False)[0]['summary_text']
-  
-        to_summarize = to_summarize[max_input_length:]
-        last_summary = curr_summary
-        print(f'summary cycle : {i}')
-        i += 1
-    print(f'final summary : {final_summary}')
-  
-  except Exception as e:
-    print(f"An error occurred: {e}")
+  comments = scrapeComments(API_Key, video_id)
+  total_comments = len(comments)
+  print("Number of comments : ", total_comments)
+  if len(comments) == 0 :
+    print("No comments found!")
+    return
+  comments = " ".join(comments)
+
+  summarizer = pipeline("summarization", model="facebook/bart-large-xsum")
+
+  config = AutoConfig.from_pretrained("facebook/bart-large-xsum")
+  max_len = config.max_position_embeddings
+
+  try :
+    summary = ''
+    if (len(comments) < max_len) :
+      summary = summarizer(comments, max_length=80, min_length=5, do_sample=False)[0]['summary_text']
+    else :
+      i = 0
+      cnt = 1
+      while i < len(comments) :
+        print(f"summararizing chunk no. : {cnt}")
+        text = comments[i : min(i + max_len, len(comments))]
+        summary += " " + summarizer(text, max_length=80, min_length=5, do_sample=False)[0]['summary_text']
+        i += max_len
+        cnt += 1
+
+      summary = summarizer(summary, max_length=100, min_length=5, do_sample=False)[0]['summary_text']
+    return summary
+  except Exception as e :
+    print(f"An error occurred : {e}")
